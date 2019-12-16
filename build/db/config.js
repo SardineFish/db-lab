b91a2496bcb9a7d39a66dcfddda76114db35ff94
SardineFish <Sardine@live.cn>
2019-12-16 14:18:22 +0800

    remove builds.

--- build/db/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DBConfig = {
-    user: "username",
-    password: "password",
-    server: "server",
-    database: "database",
-};
